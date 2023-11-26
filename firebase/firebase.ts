84eb0645261902666d9fadc1d9588842b7c4f9ea
yijiesoo <sooyijie111@gmail.com>
2023-11-26 11:49:26 +0800

    updated version so now the chat stores user data

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,31 +1,28 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getAuth, getReactNativePersistence } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+ apiKey: process.env.FIREBASE_API_KEY,
+ authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+ projectId: process.env.FIREBASE_PROJECT_ID,
+ storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+ messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+ appId: process.env.FIREBASE_APP_ID,
+ measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase services using the app instance
 const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage)
-})
+ persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+const db = getDatabase(app);
+const analytics = getAnalytics(app);
 
-export { app };
+export { app, auth, db, analytics };
--- screens/ChatRoomScreen.js
+++ screens/ChatRoomScreen.js
@@ -1,28 +1,53 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
+import { getDatabase, ref, onValue, push } from "firebase/database";
+import { auth , db } from '../firebase/firebase';
 
-function ChatRoomScreen({ route }) {
-  const { userId, userName } = route.params;
-  const [messages, setMessages] = useState([]);
+const ChatRoomScreen = ({ userID1, userID2 }) => {
+ const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    // Fetch initial chat messages from your server/database
-    // Update the 'messages' state with the fetched messages
-  }, []);
+ // Create a unique chat room ID for each pair of users
+ const sortedIDs = [userID1, userID2].sort();
+ const chatRoomID = sortedIDs.join('-');
+ console.log(chatRoomID)
+ 
+ useEffect(() => {
+ const db = getDatabase();
+ const messagesRef = ref(db, 'messages/' + chatRoomID);
+ onValue(messagesRef, snapshot => {
+ const data = snapshot.val();
+ const newMessages = Object.keys(data).map(key => ({
+   _id: key,
+   text: data[key].text,
+   createdAt: data[key].createdAt,
+   user: {
+     _id: data[key].userId,
+   },
+ }));
+ setMessages(newMessages);
+ });
+ }, [chatRoomID]);
 
-  const onSend = useCallback((newMessages = []) => {
-    setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessages));
-    
-    // Send the new messages to your server/database
-  }, []);
+ const onSend = (newMessages = []) => {
+  setMessages(GiftedChat.append(messages, newMessages));
+  newMessages.forEach((message) => {
+    push(ref(db, 'messages/' + chatRoomID), {
+      text: message.text,
+      createdAt: message.createdAt,
+      userId: message.user._id,
+    });
+  });
+};
 
-  return (
-    <GiftedChat
-      messages={messages}
-      onSend={onSend}
-      user={{ _id: userId, name: userName }}
-    />
-  );
-}
+ return (
+ <GiftedChat
+ messages={messages}
+ onSend={newMessages => onSend(newMessages)}
+ user={{
+ _id: auth.currentUser ? auth.currentUser.uid : null,
+ }}
+ />
+ );
+};
 
 export default ChatRoomScreen;
