be5a50931927da02dea27bedb53f1e94d6d90329
jianrontan <116509027+jianrontan@users.noreply.github.com>
2023-12-08 00:02:03 +0800

    Attempt at merging

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -1,101 +1,286 @@
-import React, { useState, useEffect } from 'react';
-import { View, SafeAreaView, Text, TouchableOpacity, TextInput, Image, Dimensions, StyleSheet } from 'react-native';
-import { getAuth } from 'firebase/auth';
-import { doc, updateDoc, getDoc } from 'firebase/firestore';
+import { useEffect, useState, useCallback, useMemo } from 'react';
+import { View, ScrollView, SafeAreaView, StyleSheet, Text, TouchableOpacity, TextInput, Image, Button, Dimensions } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDoc, updateDoc, doc, setDoc, addDoc, collection, onSnapshot, arrayUnion } from 'firebase/firestore';
 import { db, storage } from '../firebase/firebase';
-import { uploadBytesResumable, ref, getDownloadURL } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
+import { uploadBytesResumable, ref, getDownloadURL, deleteObject } from 'firebase/storage';
+import { StatusBar } from 'expo-status-bar';
+import DraggableFlatList from 'react-native-draggable-flatlist';
+import * as ImageManipulator from 'expo-image-manipulator';
+import * as ImagePicker from 'expo-image-picker';
+import SelectDropdown from 'react-native-select-dropdown';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-const EditProfileScreen = ({ navigation }) => {
-  const auth = getAuth();
-  const userId = auth.currentUser.uid;
+import OptionButton from '../components/touchableHighlight/touchableHightlight'
+import { COLORS, SIZES, FONT } from '../constants';
 
-  const [name, setName] = useState('');
-  const [birthday, setBirthday] = useState('');
-  const [gender, setGender] = useState('');
-  const [orientation, setOrientation] = useState({
-    male: false,
-    female: false,
-    nonBinary: false,
-  });
-  const [image, setImage] = useState([]);
-  const [progress, setProgress] = useState(0);
+export default function EditProfileScreen({ navigation }) {
+    const [userData, setUserData] = useState(null);
 
-  useEffect(() => {
-    // Fetch user profile data and set state
-    const fetchUserProfile = async () => {
-      try {
-        const userDocRef = doc(db, 'profiles', userId);
-        const userDocSnapshot = await getDoc(userDocRef);
-        const userData = userDocSnapshot.data();
+    // Authentication
+    const auth = getAuth();
+    const userId = auth.currentUser.uid;
 
-        if (userData) {
-          setName(userData.name || '');
-          setBirthday(userData.birthday || '');
-          setGender(userData.gender || '');
-          setOrientation(userData.orientation || { male: false, female: false, nonBinary: false });
-          // Assuming you have an array of imageURLs in the userData
-          setImage(userData.imageURLs || []);
-        }
-      } catch (error) {
-        console.error('Error fetching user profile:', error);
-      }
+    // Screen
+    const { width } = Dimensions.get('window');
+
+    // Orientation
+    const [orientation, setOrientation] = useState(null);
+    const [orientationError, setOrientationError] = useState('');
+    const defaultOrientation = { male: false, female: false, nonBinary: false };
+    const actualOrientation = orientation || defaultOrientation;
+
+    // Images
+    const [image, setImage] = useState([]);
+    const [progress, setProgress] = useState(0);
+    const [refreshKey, setRefreshKey] = useState(0);
+
+    // Update
+    const [error, setError] = useState('');
+
+    // Get user's data
+    useEffect(() => {
+        const getFirestoreData = async () => {
+            const docRef = doc(db, 'profiles', userId);
+            const docSnap = await getDoc(docRef);
+
+            if (docSnap.exists()) {
+                const holdData = docSnap.data();
+                setUserData(holdData);
+                setOrientation(holdData.orientation);
+                const initialImages = holdData.imageURLs.map((url, index) => ({
+                    id: Math.random().toString(),
+                    uri: url,
+                    order: index
+                }));
+                setImage(initialImages);
+            } else {
+                console.log('No such document!');
+            }
+        };
+
+        getFirestoreData();
+    }, []);
+
+    // ORIENTATION
+    const handleOrientation = (id, isSelected) => {
+        setOrientation(prevState => {
+            const newOrientation = { ...prevState, [id]: isSelected };
+            if (Object.values(newOrientation).every(option => !option)) {
+                setOrientationError('Please select at least one orientation.');
+            } else {
+                setOrientationError('');
+            }
+            return newOrientation;
+        });
     };
 
-    fetchUserProfile();
-  }, [userId]);
+    // IMAGES
+    const handleImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [3, 4],
+            quality: 0.2,
+        });
+        if (!result.canceled) {
+            let newImage = { id: Math.random().toString(), uri: result.assets[0].uri, order: image.length, isNew: true };
+            setImage(prevImages => [...prevImages, newImage]);
+        }
+    };
 
-  const handleOrientation = (id, isSelected) => {
-    setOrientation((prevState) => {
-      const newOrientation = { ...prevState, [id]: isSelected };
-      return newOrientation;
-    });
-  };
+    const uploadImage = async (uri, order, id) => {
+        const response = await fetch(uri);
+        const blob = await response.blob();
 
-  // Other image-related functions can be added similar to ProfileScreen
+        const storageRef = ref(storage, "profile_pictures/" + userId + "/" + new Date().getTime());
+        const uploadTask = uploadBytesResumable(storageRef, blob);
 
-  const handleSubmit = async () => {
-    try {
-      const userDocRef = doc(db, 'profiles', userId);
-
-      const sortedImages = [...image].sort((a, b) => a.order - b.order);
-      const imageURLs = [];
-
-      for (let img of sortedImages) {
-        const url = await uploadImage(img.uri, 'image');
-        imageURLs.push(url);
-      }
-
-      await updateDoc(userDocRef, {
-        name: name,
-        birthday: birthday,
-        gender: gender,
-        orientation: orientation,
-        imageURLs: imageURLs,
-        complete: true,
-      });
-
-      navigation.goBack(); // or navigate to another screen
-    } catch (error) {
-      console.error('Error updating user profile:', error);
+        return new Promise((resolve, reject) => {
+            uploadTask.on("state_changed",
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    console.log("Upload is " + progress + "% done")
+                    setProgress(progress.toFixed())
+                },
+                (error1) => {
+                    console.log(error1)
+                    reject(error1);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        console.log("File available at: ", downloadURL);
+                        resolve({ url: downloadURL, id: id });
+                    })
+                }
+            )
+        });
     }
-  };
 
-  return (
-    <SafeAreaView style={styles.container}>
-      {/* Render UI components for EditProfileScreen similar to ProfileScreen */}
-      {/* ... */}
-      {/* Add a "Save" button to trigger the handleSubmit function */}
-      <TouchableOpacity onPress={handleSubmit} style={styles.btnContainer}>
-        <View>
-          <Text style={styles.textStyle}>Save</Text>
-        </View>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
-};
+    const renderItem = ({ item, index, drag, isActive }) => {
+        return (
+            <GestureHandlerRootView>
+                <View
+                    style={{
+                        height: 200,
+                        backgroundColor: isActive ? 'transparent' : item.backgroundColor,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <View style={{ marginTop: 50 }}>
+                        <TouchableOpacity onLongPress={drag}>
+                            <Image key={index} source={{ uri: item.uri }} style={{ width: 150, height: 200 }} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={{ flex: 1, marginTop: 35, alignItems: 'center', justifyContent: 'center' }}>
+                    <TouchableOpacity onPress={() => removeImage(item.id)} style={{ borderWidth: 1 }}>
+                        <Text>Remove</Text>
+                    </TouchableOpacity>
+                </View>
+            </GestureHandlerRootView>
+        );
+    };
+
+    const removeImage = (id) => {
+        console.log("removeImage called")
+        const imgIndex = image.findIndex((img) => img.id === id);
+        if (imgIndex !== -1) {
+            const { uri } = image[imgIndex];
+            const desertRef = ref(storage, uri);
+            deleteObject(desertRef).then(() => {
+                console.log("File deleted successfully");
+            }).catch((error) => {
+                console.error("Error deleting file: ", error);
+            });
+    
+            setImage(prevImages => {
+                return prevImages.filter((image) => image.id !== id).map((img, index) => {
+                    img.order = index;
+                    return img;
+                });
+            });
+            setRefreshKey(oldKey => oldKey + 1);
+        }
+    };
+
+    // SUBMIT
+    const handleSubmit = async () => {
+        try {
+            const userId = auth.currentUser.uid;
+            const userDocRef = doc(db, 'profiles', userId);
+            
+            const sortedImages = [...image].sort((a,b) => a.order - b.order)
+            const imageURLs = [];
+            for (let img of sortedImages) {
+                let url;
+                if (img.isNew) {
+                    const uploadResult = await uploadImage(img.uri, img.order, img.id);
+                    url = uploadResult.url;
+                    const imgIndex = image.findIndex(i => i.id === uploadResult.id);
+                    if (imgIndex !== -1) {
+                        image[imgIndex].isNew = false;
+                    }
+                } else {
+                    url = img.uri;
+                }
+                imageURLs.push(url);
+            }
+            await updateDoc(userDocRef, {
+                orientation: orientation,
+                imageURLs: imageURLs,
+            });
+            navigation.navigate('App');
+        } catch(e) {
+            console.error("Error submitting: ", e);
+            setError(e.message);
+        }
+    };    
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <DraggableFlatList
+                style={{ flex: 1, width: width }}
+                showsVerticalScrollIndicator={false}
+                data={image}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => `draggable-item-${index}`}
+                onDragEnd={({ data }) => {
+                    data.forEach((item, index) => {
+                        item.order = index;
+                    });
+                    setImage(data);
+                }}
+                extraData={[image, refreshKey]}
+                ListHeaderComponent={
+                    <>
+                        <View style={styles.container}>
+                            {/* Orientation */}
+                            <View>
+                                {!!orientationError && <Text style={{ color: '#cf0202' }}>{orientationError}</Text>}
+                            </View>
+                            <View>
+                                <>
+                                    <OptionButton id="male" text="Male" onPress={handleOrientation} selected={actualOrientation.male} />
+                                    <OptionButton id="female" text="Female" onPress={handleOrientation} selected={actualOrientation.female} />
+                                    <OptionButton id="nonBinary" text="Non-Binary" onPress={handleOrientation} selected={actualOrientation.nonBinary} />
+                                </>
+                            </View>
+                            {/* Image */}
+                            <View>
+                                <TouchableOpacity onPress={handleImage}>
+                                    <Text style={styles.textStyle2}>Upload Image</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </>
+                }
+                ListFooterComponent={
+                    <>
+                        {/* Submit */}
+                        <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 50 }}>
+                            {!!error && <Text style={{ color: '#cf0202' }}>{error}</Text>}
+                            <TouchableOpacity activeOpacity={0.69} onPress={handleSubmit} style={styles.btnContainer}>
+                                <View>
+                                    <Text style={styles.textStyle}>Submit</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    </>
+                }
+            />
+        </SafeAreaView>
+    );
+}
 
 const styles = StyleSheet.create({
-  // Your existing styles
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    btnContainer: {
+        width: 200,
+        height: 60,
+        backgroundColor: COLORS.themeColor,
+        borderRadius: SIZES.large / 1.25,
+        borderWidth: 1.5,
+        borderColor: COLORS.white,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    textStyle: {
+        fontFamily: FONT.medium,
+        fontSize: SIZES.smallmedium,
+        color: COLORS.white,
+    },
+    textStyle2: {
+        fontFamily: FONT.medium,
+        fontSize: SIZES.smallmedium,
+        color: 'black',
+    },
 });
-
-export default EditProfileScreen;
--- screens/MatchesScreen.js
+++ screens/MatchesScreen.js
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Button } from 'react-native';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
+import ChatRoomScreen from './ChatRoomScreen'; // Import ChatRoomScreen
 
 const MatchesScreen = ({ navigation }) => {
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      try {
+        const usersCollection = collection(db, 'profiles');
+        const snapshot = await getDocs(usersCollection);
+        const usersData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+        const currentUser = usersData[0];
+
+        // Filter liked users who also liked the current user
+        const matchedUsers = usersData.filter((user) => {
+          if (user.likes && user.likedBy) {
+            // Check if the current user is in the likedBy array of the liked user
+            const likedByCurrentUser = user.likedBy.includes(currentUser.id);
+
+            // Check if the liked user is in the likes array of the current user
+            const currentUserLikesLikedUser = currentUser.likes.includes(user.id);
+
+            return likedByCurrentUser && currentUserLikesLikedUser;
+          }
+
+          return false;
+        });
+
+        setMatches(matchedUsers);
+      } catch (error) {
+        console.error('Error fetching matches:', error);
+      }
+    };
+
+    fetchMatches(); // Call the fetchMatches function when the component mounts
+  }, []);
+
   return (
     <View>
       <Text>Matches Screen</Text>
+      {matches.map((match) => (
+        <View key={match.id}>
+          <Text>Name: {match.name}</Text>
+          {/* Display other details of the matched user as needed */}
+        </View>
+      ))}
       <Button
         title="Go to Chat Room"
-        onPress={() =>
-          navigation.navigate('ChatRoom', { userId: '123', userName: 'John' })
-        }
+        onPress={() => {
+          // Navigate to the ChatRoomScreen
+          navigation.navigate('ChatRoomScreen', {
+            userId: matches[0]?.id, // Pass the user ID to ChatRoomScreen (adjust as needed)
+            userName: matches[0]?.name, // Pass the user name to ChatRoomScreen (adjust as needed)
+          });
+        }}
       />
     </View>
   );
