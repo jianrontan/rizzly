9b0fd235f040f05ae2ac70f99ac80ec336ddd12f
yijiesoo <sooyijie111@gmail.com>
2023-12-14 12:40:45 +0800

    backup copy

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -1,5 +1,7 @@
-import { useEffect, useState, useCallback, useMemo } from 'react';
-import { View, ScrollView, SafeAreaView, StyleSheet, Text, TouchableOpacity, TextInput, Image, Button, Dimensions } from 'react-native';
+import { useEffect, useState, useCallback, useRef } from 'react';
+import { View, ScrollView, SafeAreaView, StyleSheet, Text, TouchableOpacity, Alert, TextInput, Image, Button, Dimensions, BackHandler, ActivityIndicator } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import { useIsDrawerOpen } from '@react-navigation/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useSelector, useDispatch } from 'react-redux';
 import { getDoc, updateDoc, doc, setDoc, addDoc, collection, onSnapshot, arrayUnion } from 'firebase/firestore';
@@ -13,11 +15,38 @@ import * as ImagePicker from 'expo-image-picker';
 import SelectDropdown from 'react-native-select-dropdown';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
-import OptionButton from '../components/touchableHighlight/touchableHightlight'
+import { setInitialOrientation, setCurrentOrientation, setInitialImage, setCurrentImage, setHasUnsavedChangesExport } from '../redux/actions';
+import OptionButton from '../components/touchableHighlight/touchableHightlight';
 import { COLORS, SIZES, FONT } from '../constants';
 
 export default function EditProfileScreen({ navigation }) {
+
+    // All data
     const [userData, setUserData] = useState(null);
+
+    // REVERT BACK TO LOCAL STATE BUT FOR NOW IT WORKS //
+    // Redux data
+    const currentOrientationVal = useSelector(state => state.editProfileReducer.currentOrientationVal)
+    const initialOrientationVal = useSelector(state => state.editProfileReducer.initialOrientationVal)
+    const currentImageVal = useSelector(state => state.editProfileReducer.currentImageVal)
+    const initialImageVal = useSelector(state => state.editProfileReducer.initialImageVal)
+    const currentOrientationRef = useRef(null);
+    const initialOrientationRef = useRef(null);
+    const currentImageRef = useRef([]);
+    const initialImageRef = useRef([]);
+    // Redux data > latest
+    useEffect(() => {
+        currentOrientationRef.current = currentOrientationVal;
+    }, [currentOrientationVal]);
+    useEffect(() => {
+        initialOrientationRef.current = initialOrientationVal;
+    }, [initialOrientationVal]);
+    useEffect(() => {
+        currentImageRef.current = currentImageVal;
+    }, [currentImageVal]);
+    useEffect(() => {
+        initialImageRef.current = initialImageVal;
+    }, [initialImageVal]);
 
     // Authentication
     const auth = getAuth();
@@ -34,39 +63,72 @@ export default function EditProfileScreen({ navigation }) {
 
     // Images
     const [image, setImage] = useState([]);
+    const [removedImage, setRemovedImage] = useState([]);
     const [progress, setProgress] = useState(0);
     const [refreshKey, setRefreshKey] = useState(0);
 
     // Update
     const [error, setError] = useState('');
 
-    // Get user's data
-    useEffect(() => {
-        const getFirestoreData = async () => {
-            const docRef = doc(db, 'profiles', userId);
-            const docSnap = await getDoc(docRef);
+    // Changes
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
+    // Changes > Redux
+    const dispatch = useDispatch();
 
+    // Get user's data
+    const getFirestoreData = () => {
+        const docRef = doc(db, 'profiles', userId);
+        const unsubscribe = onSnapshot(docRef, (docSnap) => {
             if (docSnap.exists()) {
                 const holdData = docSnap.data();
                 setUserData(holdData);
+                dispatch(setInitialOrientation(holdData.orientation));
+                dispatch(setCurrentOrientation(holdData.orientation));
                 setOrientation(holdData.orientation);
-                const initialImages = holdData.imageURLs.map((url, index) => ({
-                    id: Math.random().toString(),
-                    uri: url,
-                    order: index
-                }));
-                setImage(initialImages);
+                if (holdData.imageURLs) {
+                    const initialImages = holdData.imageURLs.map((url, index) => ({
+                        id: Math.random().toString(),
+                        uri: url,
+                        order: index
+                    }));
+                    dispatch(setInitialImage(initialImages));
+                    dispatch(setCurrentImage(initialImages));
+                    setImage(initialImages);
+                } else {
+                    setImage([]);
+                    dispatch(setInitialImage([]));
+                }
+                setIsLoading(false);
             } else {
                 console.log('No such document!');
+                setIsLoading(false);
             }
-        };
+        });
 
-        getFirestoreData();
-    }, []);
+        // Clean up the listener when the component unmounts
+        return () => unsubscribe();
+    };
+
+    useFocusEffect(
+        useCallback(() => {
+            setIsLoading(true);
+            getFirestoreData();
+        }, [])
+    );
 
     // ORIENTATION
     const handleOrientation = (id, isSelected) => {
         setOrientation(prevState => {
+            const newOrientation = { ...prevState, [id]: isSelected };
+            if (Object.values(newOrientation).every(option => !option)) {
+                setOrientationError('Please select at least one orientation.');
+            } else {
+                setOrientationError('');
+            }
+            return newOrientation;
+        });
+        setCurrentOrientation(prevState => {
             const newOrientation = { ...prevState, [id]: isSelected };
             if (Object.values(newOrientation).every(option => !option)) {
                 setOrientationError('Please select at least one orientation.');
@@ -86,8 +148,14 @@ export default function EditProfileScreen({ navigation }) {
             quality: 0.2,
         });
         if (!result.canceled) {
-            let newImage = { id: Math.random().toString(), uri: result.assets[0].uri, order: image.length, isNew: true };
+            let newImage = {
+                id: Math.random().toString(),
+                uri: result.assets[0].uri,
+                order: image.length,
+                isNew: true, // Assume all picked images are new and local
+            };
             setImage(prevImages => [...prevImages, newImage]);
+            setCurrentImage(prevImages => [...prevImages, newImage]);
         }
     };
 
@@ -95,29 +163,30 @@ export default function EditProfileScreen({ navigation }) {
         const response = await fetch(uri);
         const blob = await response.blob();
 
-        const storageRef = ref(storage, "profile_pictures/" + userId + "/" + new Date().getTime());
+        const storageRef = ref(storage, `profile_pictures/${userId}/${Date.now()}`);
         const uploadTask = uploadBytesResumable(storageRef, blob);
 
         return new Promise((resolve, reject) => {
-            uploadTask.on("state_changed",
+            uploadTask.on(
+                "state_changed",
                 (snapshot) => {
-                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                    console.log("Upload is " + progress + "% done")
-                    setProgress(progress.toFixed())
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(`Upload is ${progress}% done`);
+                    setProgress(progress.toFixed());
                 },
-                (error1) => {
-                    console.log(error1)
-                    reject(error1);
+                (error) => {
+                    console.log(error);
+                    reject(error);
                 },
                 () => {
                     getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                        console.log("File available at: ", downloadURL);
+                        console.log(`File available at: ${downloadURL}`);
                         resolve({ url: downloadURL, id: id });
-                    })
+                    });
                 }
-            )
+            );
         });
-    }
+    };
 
     const renderItem = ({ item, index, drag, isActive }) => {
         return (
@@ -146,77 +215,122 @@ export default function EditProfileScreen({ navigation }) {
     };
 
     const removeImage = (id) => {
-        console.log("removeImage called")
         const imgIndex = image.findIndex((img) => img.id === id);
         if (imgIndex !== -1) {
-            const { uri } = image[imgIndex];
-            const desertRef = ref(storage, uri);
-            deleteObject(desertRef).then(() => {
-                console.log("File deleted successfully");
-            }).catch((error) => {
-                console.error("Error deleting file: ", error);
-            });
-    
-            setImage(prevImages => {
-                return prevImages.filter((image) => image.id !== id).map((img, index) => {
-                    img.order = index;
-                    return img;
-                });
-            });
-            setRefreshKey(oldKey => oldKey + 1);
+            const { uri, isNew } = image[imgIndex];
+            if (!isNew) {
+                setRemovedImage((oldArray) => [...oldArray, uri]);
+            }
+            setImage((prevImages) => prevImages.filter((img) => img.id !== id));
+            setCurrentImage((prevImages) => prevImages.filter((img) => img.id !== id));
+            setRefreshKey((oldKey) => oldKey + 1);
         }
     };
+
 
     // SUBMIT
     const handleSubmit = async () => {
         try {
-            const userId = auth.currentUser.uid;
             const userDocRef = doc(db, 'profiles', userId);
-            
-            const sortedImages = [...image].sort((a,b) => a.order - b.order)
+            const sortedImages = [...image].sort((a, b) => a.order - b.order);
             const imageURLs = [];
+
             for (let img of sortedImages) {
-                let url;
                 if (img.isNew) {
                     const uploadResult = await uploadImage(img.uri, img.order, img.id);
-                    url = uploadResult.url;
-                    const imgIndex = image.findIndex(i => i.id === uploadResult.id);
-                    if (imgIndex !== -1) {
-                        image[imgIndex].isNew = false;
-                    }
+                    imageURLs.push(uploadResult.url);
                 } else {
-                    url = img.uri;
+                    imageURLs.push(img.uri);
                 }
-                imageURLs.push(url);
             }
+
+            for (let url of removedImage) {
+                const deleteRef = ref(storage, url);
+                await deleteObject(deleteRef);
+            };
+
             await updateDoc(userDocRef, {
                 orientation: orientation,
                 imageURLs: imageURLs,
             });
+            setHasUnsavedChanges(false);
+            console.log("edit profile screen changed hasUnsavedChanges to false")
             navigation.navigate('App');
-        } catch(e) {
+        } catch (e) {
             console.error("Error submitting: ", e);
             setError(e.message);
         }
-    };    
+    };
+
+    // CHANGES
+    useEffect(() => {
+        if (!isLoading) {
+            if (orientation !== initialOrientationRef.current || image !== initialImageRef.current) {
+                setHasUnsavedChanges(true);
+                dispatch(setHasUnsavedChangesExport(true));
+                console.log("edit profile screen changed hasUnsavedChanges to true")
+            } else {
+                setHasUnsavedChanges(false);
+                dispatch(setHasUnsavedChangesExport(false));
+                console.log("edit profile screen changed hasUnsavedChanges to false")
+            }
+        }
+    }, [orientation, image, isLoading]);
+
+    // Hardware back button
+    useFocusEffect(
+        useCallback(() => {
+            const backAction = () => {
+                if (hasUnsavedChanges) {
+                    Alert.alert("Discard changes?", "You have unsaved changes. Are you sure you want to discard them?", [
+                        { text: "Don't leave", style: 'cancel', onPress: () => { } },
+                        {
+                            text: 'Discard',
+                            style: 'destructive',
+                            onPress: () => {
+                                setOrientation(initialOrientationVal)
+                                setImage(initialImageVal)
+                                navigation.navigate('App')
+                            },
+                        },
+                    ]);
+                    return true;
+                }
+            };
+
+            const backHandler = BackHandler.addEventListener("hardwareBackPress", backAction);
+
+            return () => backHandler.remove();
+        }, [hasUnsavedChanges, initialOrientationVal, initialImageVal, navigation])
+    );
+
+    if (isLoading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" />
+            </View>
+        );
+    }
 
     return (
-        <SafeAreaView style={styles.container}>
-            <DraggableFlatList
-                style={{ flex: 1, width: width }}
-                showsVerticalScrollIndicator={false}
-                data={image}
-                renderItem={renderItem}
-                keyExtractor={(item, index) => `draggable-item-${index}`}
-                onDragEnd={({ data }) => {
-                    data.forEach((item, index) => {
-                        item.order = index;
-                    });
-                    setImage(data);
-                }}
-                extraData={[image, refreshKey]}
-                ListHeaderComponent={
-                    <>
+        <GestureHandlerRootView style={styles.container}>
+            <SafeAreaView>
+                <DraggableFlatList
+                    style={{ flex: 1, width: width }}
+                    showsVerticalScrollIndicator={false}
+                    data={image}
+                    renderItem={renderItem}
+                    keyExtractor={(item, index) => `draggable-item-${index}`}
+                    onDragEnd={({ data }) => {
+                        const newData = [...data].map((item, index) => ({
+                            ...item,
+                            order: index,
+                        }));
+                        setImage(newData);
+                        dispatch(setCurrentImage(newData));
+                    }}
+                    extraData={[image, refreshKey]}
+                    ListHeaderComponent={() =>
                         <View style={styles.container}>
                             {/* Orientation */}
                             <View>
@@ -236,11 +350,9 @@ export default function EditProfileScreen({ navigation }) {
                                 </TouchableOpacity>
                             </View>
                         </View>
-                    </>
-                }
-                ListFooterComponent={
-                    <>
-                        {/* Submit */}
+                    }
+                    ListFooterComponent={() =>
+                        // Submit
                         <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 50 }}>
                             {!!error && <Text style={{ color: '#cf0202' }}>{error}</Text>}
                             <TouchableOpacity activeOpacity={0.69} onPress={handleSubmit} style={styles.btnContainer}>
@@ -249,10 +361,10 @@ export default function EditProfileScreen({ navigation }) {
                                 </View>
                             </TouchableOpacity>
                         </View>
-                    </>
-                }
-            />
-        </SafeAreaView>
+                    }
+                />
+            </SafeAreaView>
+        </GestureHandlerRootView>
     );
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,29 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, Button } from 'react-native';
-import { collection, getDocs, updateDoc, arrayUnion, doc } from 'firebase/firestore';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper';
+import { collection, getDocs, updateDoc, arrayUnion, doc, getDoc } from 'firebase/firestore';
 import { parse, isDate } from 'date-fns';
 import { db, auth } from '../firebase/firebase';
 
 const HomeScreen = () => {
   const [users, setUsers] = useState([]);
   const [currentUserOrientation, setCurrentUserOrientation] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // Fetch the authenticated user's profile
+        const userDoc = doc(db, 'profiles', auth.currentUser.uid);
+        const userSnapshot = await getDoc(userDoc);
+        const currentUserData = userSnapshot.exists() ? { id: userSnapshot.id, ...userSnapshot.data() } : null;
+
+        if (currentUserData && currentUserData.orientation) {
+          setCurrentUserOrientation(currentUserData.orientation);
+        }
+
+        // Fetch all profiles
         const usersCollection = collection(db, 'profiles');
         const snapshot = await getDocs(usersCollection);
         const usersData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-
-        const currentUser = usersData[0];
-        if (currentUser && currentUser.orientation) {
-          const { male, female, nonBinary } = currentUser.orientation;
-
-          if (male) setCurrentUserOrientation('male');
-          else if (female) setCurrentUserOrientation('female');
-          else if (nonBinary) setCurrentUserOrientation('nonBinary');
-          else setCurrentUserOrientation('default');
-        }
 
         // Filter users based on both gender and orientation
         const filteredUsers = usersData.filter((user) => {
@@ -34,18 +36,19 @@ const HomeScreen = () => {
             return true; // Display all users if no specific orientation is set
           }
 
+          // Modify the logic to compare with the authenticated user's orientation
           if (currentUserOrientation === 'male') {
-            return userGender === 'male';
+            return userOrientation === 'male';
           } else if (currentUserOrientation === 'female') {
-            return userGender === 'female';
+            return userOrientation === 'female';
           } else if (currentUserOrientation === 'nonbinary') {
-            return userGender === 'nonbinary';
+            return userOrientation === 'nonbinary';
           } else if (currentUserOrientation === 'maleandfemale') {
-            return userGender === 'male' || userGender === 'female';
+            return userOrientation === 'male' || userOrientation === 'female';
           } else if (currentUserOrientation === 'femaleandnonbinary') {
-            return userGender === 'female' || userGender === 'nonbinary';
+            return userOrientation === 'female' || userOrientation === 'nonbinary';
           } else if (currentUserOrientation === 'maleandnonbinary') {
-            return userGender === 'male' || userGender === 'nonbinary';
+            return userOrientation === 'male' || userOrientation === 'nonbinary';
           } else {
             return true; // Display all users if no specific criteria match
           }
@@ -80,53 +83,108 @@ const HomeScreen = () => {
 
   const handleLikeClick = async (likedUserId) => {
     try {
-      // Reference to the clicked user's document
       const likedUserDocRef = doc(db, 'profiles', likedUserId);
 
-      // Update likedBy field in the clicked user's document
       await updateDoc(likedUserDocRef, {
         likedBy: arrayUnion(auth.currentUser.uid),
       });
 
-      // Reference to the current user's document
       const currentUserDocRef = doc(db, 'profiles', auth.currentUser.uid);
 
-      // Update likes field in the current user's document
       await updateDoc(currentUserDocRef, {
         likes: arrayUnion(likedUserId),
       });
 
-      // Remove the liked user from the state
-      setUsers((prevUsers) => prevUsers.filter((user) => user.id !== likedUserId));
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % users.length);
     } catch (error) {
       console.error('Error adding like:', error);
     }
   };
 
   return (
-    <View>
-      {users.map((user) => (
-        <View key={user.id}>
-          <Text>Name: {user.name}</Text>
-          <Text>Age: {calculateAge(user.birthday)}</Text>
-          <Text>Gender: {user.gender}</Text>
-          <Text>Image: </Text>
-          {user.imageURLs && user.imageURLs.length > 0 && (
-            <Image
-              source={{ uri: user.imageURLs[0] }}
-              onLoad={() => console.log('Image loaded')}
-              onError={(error) => console.log('Error loading image: ', error)}
-              style={{ width: 100, height: 100 }}
-            />
-          )}
-          <Button
-            title="Like"
-            onPress={() => handleLikeClick(user.id)}
-          />
-        </View>
-      ))}
+    <View style={styles.container}>
+      <Swiper
+        style={styles.swiper}
+        index={currentIndex}
+        onIndexChanged={(index) => setCurrentIndex(index)}
+      >
+        {users.map((user) => (
+          <View key={user.id} style={styles.swiperItem}>
+            {user.imageURLs.map((imageUrl, index) => (
+              <Image
+                key={index}
+                source={{ uri: imageUrl }}
+                onLoad={() => console.log('Image loaded')}
+                onError={(error) => console.log('Error loading image: ', error)}
+                style={styles.image}
+              />
+            ))}
+            <View style={styles.userInfoContainer}>
+              <Text style={styles.userName}>{user.name}</Text>
+              <Text style={styles.userDetails}>{user.gender}</Text>
+              <Text style={styles.userDetails}>Age: {calculateAge(user.birthday)}</Text>
+            </View>
+          </View>
+        ))}
+      </Swiper>
+      {users[currentIndex] && (
+        <TouchableOpacity
+          style={styles.likeButton}
+          onPress={() => handleLikeClick(users[currentIndex].id)}
+        >
+          <Text style={styles.likeButtonText}>✔ Like</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  swiper: {
+    height: 200,
+  },
+  swiperItem: {
+    flex: 1,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  userInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    padding: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  userName: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  userDetails: {
+    color: 'white',
+    fontSize: 16,
+  },
+  likeButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 20,
+  },
+  likeButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default HomeScreen;
