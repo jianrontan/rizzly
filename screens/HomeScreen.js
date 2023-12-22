c2a4c99066343aa03e9d7605549a6d6ee617234c
yijiesoo <sooyijie111@gmail.com>
2023-12-23 01:09:31 +0800

    first draft of cards

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet, FlatList, SafeAreaView } from 'react-native';
-import Swiper from 'react-native-swiper';
+import { View, Text, Image, StyleSheet, SafeAreaView, TouchableOpacity, Dimensions } from 'react-native';
 import { collection, getDocs, updateDoc, arrayUnion, doc, getDoc } from 'firebase/firestore';
 import { db, auth } from '../firebase/firebase';
+import SwipeCards from 'react-native-swipe-cards-deck';
+
+const { width, height } = Dimensions.get('window');
+const cardWidth = width;
+const cardHeight = height;
 
 const HomeScreen = () => {
   const [users, setUsers] = useState([]);
   const [currentUserData, setCurrentUserData] = useState(null);
+  const [swipedUpUsers, setSwipedUpUsers] = useState([]);
 
   useEffect(() => {
     const fetchCurrentUser = async () => {
@@ -30,10 +35,10 @@ const HomeScreen = () => {
       try {
         const usersCollection = collection(db, 'profiles');
         const snapshot = await getDocs(usersCollection);
-        const usersData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        let usersData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 
         // Filter users based on gender and current user's orientation
-        const filteredUsers = usersData.filter((user) => {
+        let filteredUsers = usersData.filter((user) => {
           const userGender = user.gender?.toLowerCase?.();
 
           if (currentUserData && currentUserData.orientation) {
@@ -53,17 +58,17 @@ const HomeScreen = () => {
           return true;
         });
 
-        // Exclude the current user from the filtered list
-        const filteredUsersWithoutCurrentUser = filteredUsers.filter(user => user.id !== auth.currentUser.uid);
+        // Exclude the current user and swiped up users from the list
+        filteredUsers = filteredUsers.filter(user => user.id !== auth.currentUser.uid && !swipedUpUsers.includes(user.id));
 
-        setUsers(filteredUsersWithoutCurrentUser);
+        setUsers(filteredUsers);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
     fetchData();
-  }, [currentUserData]);
+  }, [currentUserData, swipedUpUsers]);
 
   const handleLikeClick = async (likedUserId) => {
     try {
@@ -84,43 +89,65 @@ const HomeScreen = () => {
     }
   };
 
-  const renderItem = ({ item, index}) => (
-    <View style={styles.swiperItem}>
-      <Swiper
- style={[styles.swiper, { height: '70%' }]}
- index={0}
- loop={false}
->
- {item.imageURLs.map((imageUrl, imageIndex) => (
- <View key={imageIndex}>
-  <Image
-    source={{ uri: imageUrl }}
-    onLoad={() => console.log('Image loaded')}
-    onError={(error) => console.log('Error loading image: ', error)}
-    style={styles.image}
-  />
-  <View style={styles.userInfoContainer}>
-    <Text style={styles.userName}>{item.name}</Text>
-    <Text style={styles.userDetails}>{item.gender}</Text>
-    <Text style={styles.userAge}>Age: {item.age}</Text>
-  </View>
- </View>
- ))}
-</Swiper>
-
-    </View>
-  );
-  
-  
-
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
-        data={users}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        pagingEnabled={true}
-      />
+      {users.map((user, index) => (
+        <View key={index} style={styles.swiperItem}>
+          <SwipeCards
+            cards={user.imageURLs.map((imageUrl) => ({
+              image: imageUrl,
+              name: user.name,
+              details: `${user.gender}, Age: ${user.age}`,
+            }))}
+            renderCard={(card) => (
+              <View style={styles.cardContainer}>
+                <Image
+                  source={{ uri: card.image }}
+                  onLoad={() => console.log('Image loaded')}
+                  onError={(error) => console.log('Error loading image: ', error)}
+                  style={styles.image}
+                />
+                <View style={styles.userInfoContainer}>
+                  <Text style={styles.userName}>{card.name}</Text>
+                  <Text style={styles.userDetails}>{card.details}</Text>
+                  <TouchableOpacity onPress={() => handleLikeClick(card.id)}>
+                    <Text style={styles.likeButton}>Like</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            )}
+            containerStyle={styles.swipeCardsContainer}
+            renderNoMoreCards={() => (
+              <View style={styles.noMoreUsers}>
+                <Text style={styles.noMoreUsersText}>No more users left</Text>
+              </View>
+            )}
+            stackSize={3}
+            animateOverlayLabelsOpacity
+            animateCardOpacity
+            smoothTransition
+            stackSeparation={15}
+            overlayLabels={{
+              top: {
+                title: 'DISLIKE',
+                style: {
+                  label: {
+                    backgroundColor: 'red',
+                    borderColor: 'black',
+                    color: 'white',
+                    borderWidth: 1,
+                  },
+                  wrapper: {
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  },
+                },
+              },
+            }}
+          />
+        </View>
+      ))}
     </SafeAreaView>
   );
 };
@@ -129,27 +156,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  swiper:{
-    height: '70%',
+  likeButton: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    backgroundColor: 'green',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+    color: 'white',
   },
+
   swiperItem: {
     flex: 1,
-    height:'auto',
+  },
+  cardContainer: {
+    flex: 1,
+    borderRadius: 10,
+    overflow: 'hidden',
+    backgroundColor: 'white', // Set the background color of the cards
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
   image: {
-    width: '100%',
-    height: '70%', // Adjusted to take 100% of the swiper
+    flex: 1,
   },
   userInfoContainer: {
-    height: '30%', // Adjusted to take 20% of the screen
+    position: 'absolute',
     bottom: 0,
     left: 0,
     right: 0,
+    height: '30%',
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
     padding: 10,
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
   },
+
   userName: {
     color: 'white',
     fontSize: 18,
