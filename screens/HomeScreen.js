8ac70e7140a35d21ebb5b58fa36a87f050fc3c9e
yijiesoo <sooyijie111@gmail.com>
2024-01-08 17:11:22 +0800

    newest sorry

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,169 +1,167 @@
 import React, { useEffect, useState } from 'react';
 import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  SafeAreaView,
-  TouchableOpacity,
-  FlatList,
-  Dimensions,
+ View,
+ Text,
+ Image,
+ StyleSheet,
+ SafeAreaView,
+ TouchableOpacity,
+ FlatList,
+ Dimensions,
 } from 'react-native';
 import { collection, getDocs, updateDoc, arrayUnion, doc, getDoc, arrayRemove } from 'firebase/firestore';
 import { db, auth } from '../firebase/firebase';
 import Swiper from 'react-native-swiper';
 import { Swipeable } from 'react-native-gesture-handler';
 import NoMoreUserScreen from './NoMoreUserScreen';
-import { useNavigation } from 'expo-router';
-
 const { width, height } = Dimensions.get('window');
 const cardWidth = width;
 const cardHeight = height - 170 ;
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
-  const [users, setUsers] = useState([]);
-  const [currentUserData, setCurrentUserData] = useState(null);
-  const [swipedUpUsers, setSwipedUpUsers] = useState([]);
-  const [scrollOffset, setScrollOffset] = useState(0);
-  const [previousIndex, setPreviousIndex] = useState(0);
-  const [fetchedUsers, setFetchedUsers] = useState([]);
+ const [users, setUsers] = useState([]);
+ const [currentUserData, setCurrentUserData] = useState(null);
+ const [swipedUpUsers, setSwipedUpUsers] = useState([]);
+ const [scrollOffset, setScrollOffset] = useState(0);
+ const [previousIndex, setPreviousIndex] = useState(0);
+ const [fetchedUsers, setFetchedUsers] = useState([]);
 
-  useEffect(() => {
-    const fetchCurrentUser = async () => {
-      try {
-        const currentUserDocRef = doc(db, 'profiles', auth.currentUser.uid);
-        const currentUserDoc = await getDoc(currentUserDocRef);
+ useEffect(() => {
+  const fetchCurrentUser = async () => {
+    try {
+      const currentUserDocRef = doc(db, 'profiles', auth.currentUser.uid);
+      const currentUserDoc = await getDoc(currentUserDocRef);
 
-        if (currentUserDoc.exists()) {
-          setCurrentUserData(currentUserDoc.data());
-        }
-      } catch (error) {
-        console.error('Error fetching current user data:', error);
+      if (currentUserDoc.exists()) {
+        setCurrentUserData(currentUserDoc.data());
       }
-    };
+    } catch (error) {
+      console.error('Error fetching current user data:', error);
+    }
+  };
 
-    fetchCurrentUser();
-  }, []);
+  fetchCurrentUser();
+ }, []);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const usersCollection = collection(db, 'profiles');
-        const snapshot = await getDocs(usersCollection);
-        let usersData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-
-        // Filter users based on gender and current user's orientation
-        let filteredUsers = usersData.filter((user) => {
-          const userGender = user.gender?.toLowerCase?.();
-
-          if (currentUserData && currentUserData.orientation) {
-            const { male, female, nonBinary } = currentUserData.orientation;
-
-            if (userGender === 'female' && female) {
-              return true;
-            } else if (userGender === 'male' && male) {
-              return true;
-            } else if (userGender === 'nonbinary' && nonBinary) {
-              return true;
-            } else {
-              return false;
-            }
+ useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const usersCollection = collection(db, 'profiles');
+      const snapshot = await getDocs(usersCollection);
+      let usersData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+ 
+      // Filter users based on gender and current user's orientation
+      let filteredUsers = usersData.filter((user) => {
+        const userGender = user.gender?.toLowerCase?.();
+ 
+        if (currentUserData && currentUserData.orientation) {
+          const { male, female, nonBinary } = currentUserData.orientation;
+ 
+          if (userGender === 'female' && female) {
+            return true;
+          } else if (userGender === 'male' && male) {
+            return true;
+          } else if (userGender === 'nonbinary' && nonBinary) {
+            return true;
+          } else {
+            return false;
           }
-
-          return true;
-        });
-
-        // Exclude the current user and swiped up users from the list
-        filteredUsers = filteredUsers.filter(
-          (user) => user.id !== auth.currentUser.uid && !swipedUpUsers.includes(user.id)
-        );
-
-        // Always include the "No More Users" item at the end of the users array
-        setUsers([...filteredUsers ]);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, [currentUserData, swipedUpUsers]);
-
-  const handleLikeClick = async (likedUserId) => {
-    try {
-      const likedUserDocRef = doc(db, 'profiles', likedUserId);
-
-      await updateDoc(likedUserDocRef, {
-        likedBy: arrayUnion(auth.currentUser.uid),
+        }
+ 
+        return true;
       });
-
-      const currentUserDocRef = doc(db, 'profiles', auth.currentUser.uid);
-
-      await updateDoc(currentUserDocRef, {
-        likes: arrayUnion(likedUserId),
-      });
-
-      // Remove the liked user from the users array
-      setUsers((prevUsers) => prevUsers.filter((user) => user.id !== likedUserId));
-    } catch (error) {
-      console.error('Error adding like:', error);
-    }
-  };
-
-  const handleDislikeClick = async (dislikedUserId) => {
-    try {
-      const currentUserDocRef = doc(db, 'profiles', auth.currentUser.uid);
-
-      // Add the disliked user to the current user's document
-      await updateDoc(currentUserDocRef, {
-        dislikes: arrayUnion(dislikedUserId),
-      });
-
-      // Add the disliked user to the swipedUpUsers array
-      setSwipedUpUsers((prevSwipedUpUsers) => [...prevSwipedUpUsers, dislikedUserId]);
-
-      // After 10 seconds, remove the disliked user from the current user's document
-      setTimeout(async () => {
-        await updateDoc(currentUserDocRef, {
-          dislikes: arrayRemove(dislikedUserId),
-        });
-
-        // Also remove the disliked user from the swipedUpUsers array
-        setSwipedUpUsers((prevSwipedUpUsers) => prevSwipedUpUsers.filter(userId => userId !== dislikedUserId));
-      }, 10000);
-    } catch (error) {
-      console.error('Error adding dislike:', error);
-    }
-  };
-
-  const handleScroll = (event) => {
-    const offsetY = event.nativeEvent.contentOffset.y;
-
-    // Calculate the current index based on the scroll offset
-    const currentIndex = Math.round(offsetY / cardHeight);
-
-    // If the current index is greater than the previous index, it means the user has swiped to the next user
-    if (currentIndex > previousIndex) {
-      const dislikedUserId = users[previousIndex]?.id;
-
-      if (dislikedUserId) {
-        handleDislikeClick(dislikedUserId);
-      }
-    }
-
-    // Update the previous index and scroll offset
-    setPreviousIndex(currentIndex);
-    setScrollOffset(offsetY);
-  };
-
-  const renderItem = ({ item: user }) => {
-    if (user.id === 'no-more-users') {
-      return (
-        <View style={{ width: cardWidth, height: cardHeight }}>
-          <NoMoreUserScreen />
-        </View>
+ 
+      // Exclude the current user and swiped up users from the list
+      filteredUsers = filteredUsers.filter(
+        (user) => user.id !== auth.currentUser.uid && !swipedUpUsers.includes(user.id)
       );
+ 
+      // Always include the "No More Users" item at the end of the users array
+      setUsers([...filteredUsers ]);
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
+  };
+ 
+  fetchData();
+ }, [currentUserData, swipedUpUsers]);
+ 
+ const handleLikeClick = async (likedUserId) => {
+  try {
+    const likedUserDocRef = doc(db, 'profiles', likedUserId);
+ 
+    await updateDoc(likedUserDocRef, {
+      likedBy: arrayUnion(auth.currentUser.uid),
+    });
+ 
+    const currentUserDocRef = doc(db, 'profiles', auth.currentUser.uid);
+ 
+    await updateDoc(currentUserDocRef, {
+      likes: arrayUnion(likedUserId),
+    });
+ 
+    // Remove the liked user from the users array
+    setUsers((prevUsers) => prevUsers.filter((user) => user.id !== likedUserId));
+  } catch (error) {
+    console.error('Error adding like:', error);
+  }
+ }; 
+
+ const handleDislikeClick = async (dislikedUserId) => {
+  try {
+   const currentUserDocRef = doc(db, 'profiles', auth.currentUser.uid);
+ 
+   // Add the disliked user to the current user's document
+   await updateDoc(currentUserDocRef, {
+     dislikes: arrayUnion(dislikedUserId),
+   });
+ 
+   // Add the disliked user to the swipedUpUsers array
+   setSwipedUpUsers((prevSwipedUpUsers) => [...prevSwipedUpUsers, dislikedUserId]);
+ 
+   // After 10 seconds, remove the disliked user from the current user's document
+   setTimeout(async () => {
+     await updateDoc(currentUserDocRef, {
+       dislikes: arrayRemove(dislikedUserId),
+     });
+ 
+     // Also remove the disliked user from the swipedUpUsers array
+     setSwipedUpUsers((prevSwipedUpUsers) => prevSwipedUpUsers.filter(userId => userId !== dislikedUserId));
+   }, 10000);
+  } catch (error) {
+   console.error('Error adding dislike:', error);
+  }
+ };
+ 
+
+ const handleScroll = (event) => {
+  const offsetY = event.nativeEvent.contentOffset.y;
+
+  // Calculate the current index based on the scroll offset
+  const currentIndex = Math.round(offsetY / cardHeight);
+
+  // If the current index is greater than the previous index, it means the user has swiped to the next user
+  if (currentIndex > previousIndex) {
+    const dislikedUserId = users[previousIndex]?.id;
+
+    if (dislikedUserId) {
+      handleDislikeClick(dislikedUserId);
+    }
+  }
+
+  // Update the previous index and scroll offset
+  setPreviousIndex(currentIndex);
+  setScrollOffset(offsetY);
+ };
+ 
+ const renderItem = ({ item: user }) => {
+  if (user.id === 'no-more-users') {
+    return (
+      <View style={{ width: cardWidth, height: cardHeight }}>
+        <NoMoreUserScreen />
+      </View>
+    );
+  }
 
   const allImages = user.selfieURLs ? [user.selfieURLs, ...user.imageURLs] : user.imageURLs;
 
