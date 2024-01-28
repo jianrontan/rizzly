5f5e29388085f59d11a0303a2c42efc63874125e
yijiesoo <sooyijie111@gmail.com>
2024-01-28 19:44:27 +0800

    fixed static issues

--- screens/NoMoreUserScreen.js
+++ screens/NoMoreUserScreen.js
@@ -5,6 +5,7 @@ const NoMoreUserScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>No more users</Text>
+      <Text style={styles.text}> Perhaps try a different approach? Use filters to find the exact type of rizzler you want!</Text>
     </View>
   );
 };
