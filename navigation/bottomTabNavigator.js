9b509280a1d8a8dde6be48a19c79e59f0057b177
yijiesoo <sooyijie111@gmail.com>
2024-02-27 14:04:00 +0800

    changes for back btn in btmtabnavigator

--- navigation/bottomTabNavigator.js
+++ navigation/bottomTabNavigator.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { HeaderBackButton } from "@react-navigation/elements";
 import { useSelector } from 'react-redux';
 
 import Home from "../screens/HomeScreen";
 import ChatStack from "./chatNavigator";
+import MatchesScreen from "../screens/MatchesScreen";
+import ChatRoomScreen from "../screens/ChatRoomScreen";
+import ViewOtherProfile from "../screens/ViewOtherProfile";
 import LikesScreen from "../screens/LikesScreen";
 import ScreenHeaderBtn from "../components/button/ScreenHeaderBtn";
 import { COLORS, FONT, icons } from "../constants";
@@ -16,6 +20,26 @@ const Tab = createBottomTabNavigator();
 export default function BottomTabStack({ navigation }) {
     const likesCount = useSelector(state => state.editProfileReducer.likesVal);
     const unreadChatsCount = useSelector(state => state.editProfileReducer.chatVal);
+    const matches = useSelector(state => state.editProfileReducer.matchesVal);
+
+    const CustomHeaderTitle = ({ userFirstName, imageUrl, navigation, matchId }) => (
+        <TouchableOpacity
+            style={{ flexDirection: 'row', alignItems: 'center' }}
+            onPress={() => navigation.navigate('ViewOtherProfile', { matchId })}
+        >
+            <Image
+                style={{
+                    width: 50,
+                    height: 50,
+                    borderRadius: 25,
+                    marginLeft: 0,
+                    marginRight: 10,
+                }}
+                source={{ uri: imageUrl }}
+            />
+            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>{userFirstName}</Text>
+        </TouchableOpacity>
+    );
 
     return (
         <Tab.Navigator
@@ -77,7 +101,7 @@ export default function BottomTabStack({ navigation }) {
             />
             <Tab.Screen
                 name="Chats"
-                component={ChatStack}
+                component={MatchesScreen}
                 options={{
                     tabBarIcon: ({ color, size }) => (
                         <View style={{ flexDirection: 'column', alignItems: 'center' }}>
@@ -88,8 +112,74 @@ export default function BottomTabStack({ navigation }) {
                         </View>
                     ),
                     tabBarLabel: 'Chats',
-                    tabBarLabelStyle:{ color: 'white' },
+                    tabBarLabelStyle: { color: 'white' },
                     tabBarActiveTintColor: 'white',
+                }}
+            />
+            <Tab.Screen
+                name="ChatRoom"
+                component={ChatRoomScreen}
+                options={({ route, navigation }) => {
+                    const currentMatchId = route.params?.userId;
+                    const currentMatch = matches.find(match => match.id === currentMatchId);
+                    const userFirstName = currentMatch ? currentMatch.firstName : '';
+                    const imageUrl = currentMatch && currentMatch.imageURLs && currentMatch.imageURLs.length > 0
+                        ? currentMatch.imageURLs[0]
+                        : null;
+
+                    return {
+                        tabBarButton: () => null,
+                        headerLeft: () => (
+                            <HeaderBackButton onPress={() => navigation.navigate('Chats')} />
+                        ),
+                        headerTitle: () => <CustomHeaderTitle userFirstName={userFirstName} imageUrl={imageUrl} navigation={navigation} matchId={currentMatchId} />,
+                        headerRight: () => (
+                            <TouchableOpacity
+                                style={{
+                                    backgroundColor: '#D3A042',
+                                    borderRadius: 5,
+                                    padding: 10,
+                                    marginRight: 10,
+                                }}
+                                onPress={() => {
+                                    Alert.alert(
+                                        'Report Options',
+                                        '',
+                                        [
+                                            {
+                                                text: 'This is not the real person!',
+                                                onPress: () => navigation.navigate('Report'),
+                                            },
+                                            {
+                                                text: 'Inappropriate content/ Harassment',
+                                                onPress: () => navigation.navigate('Report'),
+                                            },
+                                            {
+                                                text: 'Safety issues',
+                                                onPress: () => navigation.navigate('Report'),
+                                            },
+                                            {
+                                                text: 'Cancel',
+                                                style: 'cancel',
+                                            },
+                                        ],
+                                    );
+                                }}
+                            >
+                                <Text style={{ color: 'white', fontWeight: 'bold' }}>Report</Text>
+                            </TouchableOpacity>
+                        ),
+                    };
+                }}
+            />
+            <Tab.Screen
+                name="ViewOtherProfile"
+                component={ViewOtherProfile}
+                options={{
+                    tabBarButton: () => null,
+                    headerLeft: () => (
+                        <HeaderBackButton onPress={() => navigation.goBack()} />
+                    ),
                 }}
             />
         </Tab.Navigator>
